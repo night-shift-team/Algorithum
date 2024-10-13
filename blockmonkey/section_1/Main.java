c97574d8029863d62f408aecd82a075856849a23
blockmonkey <blockmonkey@naver.com>
2024-10-13 15:17:21 +0900

    feat: [blockmonkey] s1 - p2 대소문자 뒤집기 풀이

--- blockmonkey/section_1/Main.java
+++ blockmonkey/section_1/Main.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 class Main {
     public static void main(String[] args) {
         String inputString = "Computercoorler";
@@ -7,5 +5,9 @@ class Main {
 
         FindText p1 = new FindText();
         System.out.println(p1.solution(inputString, findText));
+
+        String p2Input = "StuDY";
+        ChangeUpperAndLowerCase p2 = new ChangeUpperAndLowerCase();
+        System.out.println(p2.solution(p2Input));
     }
 }
