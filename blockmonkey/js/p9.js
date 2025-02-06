2a86f31f3afe5310446790748c6918ea5cf37745
blockmonkey <blockmonkey@naver.com>
2025-02-06 11:04:28 +0900

    feat: 9번 문제 효율성 개선

--- blockmonkey/js/p9.js
+++ blockmonkey/js/p9.js
@@ -10,6 +10,26 @@ function solution(participant, completion) {
      return participant[0];
 }
 
+function solution2(participant, completion) {
+  const map = new Map();
+
+  participant.forEach((p, i) => {
+    const eValue = map.get(p);
+    map.set(p, (eValue || 0) + 1);
+  });
+
+  completion.forEach((c, i) => {
+    const eValue = map.get(c);
+    map.set(c, eValue - 1);
+  });
+
+  for (const [k, v] of map) {
+    if (v > 0) {
+      return k;
+    }
+  }
+}
+
 
 console.log(
   `정답 : ${solution(
